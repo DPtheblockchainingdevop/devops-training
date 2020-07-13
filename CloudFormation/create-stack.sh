eb2508ce8a4ab4177eceb999e91a1b3a15f027ed
DPtheblockchainingdevop <devesco.pierre@gmail.com>
2020-07-12 22:36:09 -0600

    Updated by adding json props file

--- CloudFormation/create-stack.sh
+++ CloudFormation/create-stack.sh
@@ -2,4 +2,9 @@
 #Shelll script for aws cloudformation
 #2020 July 12
 
-aws cloudformation create-stack --stack-name $1 --template-body file://$2 --region=us-west-2 #--parameters file://$ --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+aws cloudformation create-stack \
+--stack-name $1 \
+--template-body file://$2 \
+--region=us-west-2 \
+--parameters file://$3
+# --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
