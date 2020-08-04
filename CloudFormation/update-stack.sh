04abb0ef42d31508d51c1425a1d732f736655e8f
DeVesco Pierre <68120998+DPtheblockchainingdevop@users.noreply.github.com>
2020-08-04 15:10:36 -0600

    Update script to include IAM_ROLES

--- CloudFormation/update-stack.sh
+++ CloudFormation/update-stack.sh
@@ -6,5 +6,5 @@ aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --region=us-west-2 \
---parameters file://$3 
-# --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+--parameters file://$3 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
